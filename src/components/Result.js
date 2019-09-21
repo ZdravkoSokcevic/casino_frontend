71504a577beba988d542a7e4e1a1e84ef5ade10b
ZdravkoSokcevic <zdravkosokcevic100@gmail.com>
2019-09-21 14:48:19 +0200

    initial commit

---
+++ src/components/Result.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Result extends React.Component {
+    render(){
+        return (
+            <div className="Result">
+                Rezultat
+            </div>
+        );
+    }
+}
+
+export default Result;
